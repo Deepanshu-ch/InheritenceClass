3a60d20940a9fbb258696ad109e06079dd1aecad
Deepanshu-ch <55156192+Deepanshu-ch@users.noreply.github.com>
2023-02-15 13:53:13 +0530

    first commit

---
+++ InheritenceClass/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceClass
+{
+    class Chef
+    {
+        public void MakeRajma()
+        {
+            Console.WriteLine("Chef makes Rajma");
+        }
+        public void MakeChicken()
+        {
+            Console.WriteLine("Chef makes Chicken");
+        }
+        public virtual void ChefSpecial()
+        {
+            Console.WriteLine("Chef makes best Rajma");
+        }
+
+    }
+
+    class IndianChef : Chef
+    {
+        public void MakeNaan()
+        {
+            Console.WriteLine("Chef makes Aloo Naan");
+        }
+
+        public void MakePavBhaji()
+        {
+            Console.WriteLine("Chef makes PavBhaji");
+        }
+
+        public override void ChefSpecial()
+        {
+            Console.WriteLine("Chef makes best PavBhaji");
+        }
+    }
+
+    class SouthIndianChef : IndianChef
+    {
+        public void MakeIdli()
+        {
+            Console.WriteLine("Chef makes Idli");
+        }
+
+        public void MakeDosa()
+        {
+            Console.WriteLine("Chef make Dosa");
+        }
+        public virtual void ChefSpecial()
+        {
+            Console.WriteLine("Chef makes best Masala Dosa");
+        }
+    }
+}
---
+++ InheritenceClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using InheritenceClass;
+using System;
+
+class InharitanceExample
+{    public static void Main(string[] args)
+    {
+        Chef vikram = new Chef();
+        vikram.MakeChicken();
+        vikram.MakeRajma();
+        vikram.ChefSpecial();
+
+        IndianChef Ram = new IndianChef();
+        Ram.MakeChicken();
+        Ram.MakeRajma();
+        Ram.MakeNaan();
+        Ram.ChefSpecial();
+
+        SouthIndianChef RamaRaju = new SouthIndianChef();
+        RamaRaju.MakeNaan();
+        RamaRaju.ChefSpecial();
+        RamaRaju.MakeRajma();
+
+    }
+}
